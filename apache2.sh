abc65f3849dcdfdfe74f86edeef3191fa00584aa
szasz-roland <szaszroland11@gmail.com>
2025-03-08 17:21:30 +0100

    apache2.sh hozzáadása

---
+++ apache2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Megnézi hogy sudo-val van-e futtatva
+if [ "$EUID" -ne 0 ]; then
+	echo -e "\n ⛔sudo-val futtasd te bolond🥱 ⛔ \n"
+	exit 1
+fi
+
+if $(whcich apache2); then
+	echo -e "\n Már telepítve van az apache2 \n"
+else
+	apt update && apt install apache2 -y && systemctl enable apache2 && systemctl status apache2
+fi
